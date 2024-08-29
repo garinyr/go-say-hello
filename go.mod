6f0a4dc6e906b63183ee1ce1f72ff10897c37e28
Garinyr <garin.ramadika@nobubank.com>
2024-08-29 11:11:38 +0700

    update major

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/garinyr/go-say-hello
+module github.com/garinyr/go-say-hello/v2
 
 go 1.21.5
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,12 @@
 package go_say_hello
 
-func SayHello(name string) string {
-	return "Hello " + name
+import "fmt"
+
+type Customer struct {
+	Name, Address string
+	Age           int
+}
+
+func (customer Customer) sayHello(name string) {
+	fmt.Println("Hello", name, "my name is", customer.Name)
 }
